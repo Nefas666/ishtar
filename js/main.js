f1676330549fef83f81d08f851a30d76baa546f1
Nefas666 <78368042+Nefas666@users.noreply.github.com>
2021-05-17 01:22:28 +0200

    ++mods

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ window.onload = async function() {
         .setTracker('clmtrackr')
         .setGazeListener(function(data, clock) {
              console.log(data); /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
-          // console.log(clock); /* elapsed time in milliseconds since webgazer.begin() was called */
+          //console.log(clock); /* elapsed time in milliseconds since webgazer.begin() was called */
         }).begin();
         webgazer.showVideoPreview(true) /* shows all video previews */
             .showPredictionPoints(true) /* shows a square every 100 milliseconds where current prediction is */
